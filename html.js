da316ad311f35ed63e7c0719a6a88fe099276504
Oleg Legun <oleg.legun@gmail.com>
2017-03-06 04:56:33 +0300

    Fix <meta> closing bracket

--- html.js
+++ html.js
@@ -24,7 +24,7 @@ module.exports = React.createClass({
                 <meta charSet="utf-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="apple-mobile-web-app-capable" content="yes">
+                <meta name="apple-mobile-web-app-capable" content="yes" />
                 {head.title.toComponent()}
                 {head.meta.toComponent()}
                 <link href="https://fonts.googleapis.com/css?family=Arsenal" rel="stylesheet" />
