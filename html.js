4ca0ae984fdcaac50187aa69fae414263b692c10
Oleg Legun <oleg.legun@gmail.com>
2017-03-06 04:21:49 +0300

    Add styles for PageContent

--- html.js
+++ html.js
@@ -2,9 +2,6 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { prefixLink } from 'gatsby-helpers'
 
-// import { TypographyStyle, GoogleFont } from 'react-typography'
-// import typography from './utils/typography'
-
 const BUILD_TIME = new Date().getTime()
 
 module.exports = React.createClass({
@@ -29,33 +26,32 @@ module.exports = React.createClass({
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 {head.title.toComponent()}
                 {head.meta.toComponent()}
-                {/*<TypographyStyle typography={typography} />*/}
                 <link href="https://fonts.googleapis.com/css?family=Arsenal" rel="stylesheet" />
-                {/*<GoogleFont typography={typography} />*/}
                 {css}
-                <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
-                <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
-                <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
-                <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
-                <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
-                <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
-                <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
-                <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
+
+                <link rel="apple-touch-icon" sizes="57x57" href={prefixLink('/apple-icon-57x57.png')} />
+                <link rel="apple-touch-icon" sizes="60x60" href={prefixLink('/apple-icon-60x60.png')} />
+                <link rel="apple-touch-icon" sizes="72x72" href={prefixLink('/apple-icon-72x72.png')} />
+                <link rel="apple-touch-icon" sizes="76x76" href={prefixLink('/apple-icon-76x76.png')} />
+                <link rel="apple-touch-icon" sizes="114x114" href={prefixLink('/apple-icon-114x114.png')} />
+                <link rel="apple-touch-icon" sizes="120x120" href={prefixLink('/apple-icon-120x120.png')} />
+                <link rel="apple-touch-icon" sizes="144x144" href={prefixLink('/apple-icon-144x144.png')} />
+                <link rel="apple-touch-icon" sizes="152x152" href={prefixLink('/apple-icon-152x152.png')} />
                 <link rel="apple-touch-icon" sizes="180x180"
-                      href="/favicon/apple-icon-180x180.png" />
+                      href={prefixLink('/apple-icon-180x180.png')} />
                 <link rel="icon" type="image/png" sizes="192x192"
-                      href="/favicon/android-icon-192x192.png" />
+                      href={prefixLink('/android-icon-192x192.png')} />
                 <link rel="icon" type="image/png" sizes="32x32"
-                      href="/favicon/favicon-32x32.png" />
+                      href={prefixLink('/favicon-32x32.png')} />
                 <link rel="icon" type="image/png" sizes="96x96"
-                      href="/favicon/favicon-96x96.png" />
+                      href={prefixLink('/favicon-96x96.png')} />
                 <link rel="icon" type="image/png" sizes="16x16"
-                      href="/favicon/favicon-16x16.png" />
-                <link rel="manifest" href="/manifest.json" />
+                      href={prefixLink('/favicon-16x16.png')} />
+                <link rel="manifest" href={prefixLink('/manifest.json')} />
                 <meta name="msapplication-TileColor"
                       content="#ffffff" />
                 <meta name="msapplication-TileImage"
-                      content="/favicon/ms-icon-144x144.png" />
+                      content="ms-icon-144x144.png" />
                 <meta name="theme-color"
                       content="#ffffff" />
             </head>
