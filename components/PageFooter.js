01b553e382b0cf09cf0c65157f8606a1d712f8e9
Oleg Legun <oleg.legun@gmail.com>
2017-03-08 22:03:41 +0300

    Add :hover color to price table; Fix some procedures names

--- components/PageFooter.js
+++ components/PageFooter.js
@@ -6,7 +6,7 @@ class PageFooter extends Component {
         return (
             <div className="PageFooter">
                 <div className="PageFooter__wrapper">
-                    Лицензия на осуществление медицинской деятельности <a href={prefixLink('/static/pdf/license.pdf')} target="_blank" rel="noopener noreferrer">№ ЛО-77-01-013289</a> от 18.10.2016 г.
+                    Лицензия на осуществление медицинской деятельности <a href={prefixLink('/static/pdf/licence.pdf')} target="_blank" rel="noopener noreferrer">№ ЛО-77-01-013289</a> от 18.10.2016 г.
                 </div>
             </div>
         )
