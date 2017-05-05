bf7d113292a59c0a4a8a673a6048c547de53bc0a
Oleg Legun <oleg.legun@gmail.com>
2017-05-05 17:31:42 +0300

    add styles for ContactsTabSelectPanel; edit ContactsInfoPanel contents

--- components/contacts/ContactsInfoPanel.js
+++ components/contacts/ContactsInfoPanel.js
@@ -2,7 +2,21 @@ import React, { PropTypes } from 'react'
 
 function ContactsInfoPanel() {
     return (
-        <div className="ContactsInfoPanel">ContactsInfoPanel</div>
+        <div className="ContactsInfoPanel">ContactsInfoPanel
+            <div className="ContactsInfoPanel__phone">
+                <div className="ContactsInfoPanel__icon ContactsInfoPanel__icon--phone">Phone</div>
+                <div className="ContactsInfoPanel__phone">8 (905) 588 13 13</div>
+            </div>
+            <div className="ContactsInfoPanel__address">
+                <div className="ContactsInfoPanel__icon ContactsInfoPanel__icon--address">Address</div>
+                <div className="ContactsInfoPanel__address">Москва, Рыбников переулок, дом 4</div>
+
+            </div>
+            <div className="ContactsInfoPanel__email">
+                <div className="ContactsInfoPanel__icon ContactsInfoPanel__icon--email">Email</div>
+                <div className="ContactsInfoPanel__email">info@lasertime.ru</div>
+            </div>
+        </div>
     )
 }
 
--- components/contacts/ContactsTabSelectPanel.js
+++ components/contacts/ContactsTabSelectPanel.js
@@ -2,27 +2,29 @@ import React, { PropTypes } from 'react'
 
 function ContactsTabSelectPanel(props) {
     return (
-        <div className="ContactsTabSelect">
-            ContactsTabSelectPanel
-            <div className="ContactsTabSelect__group">
-                <label className="ContactsTabSelect__option ContactsTabSelect__option--active">
-                    <input type="radio" name="ContactsTabSelect" />
-                    пешком по карте
-                </label>
-                <label className="ContactsTabSelect__option">
+        <div className="ContactsTabSelectPanel">
+
+            <label className="ContactsTabSelectPanel__option ContactsTabSelectPanel__option--active ContactsTabSelectPanel__option--hovered">
+                <input type="radio" name="ContactsTabSelect" />
+                пешком по карте
+            </label>
+
+            <div className="ContactsTabSelectPanelGroup ContactsTabSelectPanelGroup--opened">
+                <label className="ContactsTabSelectPanelGroup__option">
                     <input type="radio" name="ContactsTabSelect" />
                     пешком по фото
                 </label>
-                <label className="ContactsTabSelect__option">
+                <label className="ContactsTabSelectPanelGroup__option">
                     <input type="radio" name="ContactsTabSelect" />
                     пешком по видео
                 </label>
             </div>
-            <label className="ContactsTabSelect__option">
+
+            <label className="ContactsTabSelectPanel__option">
                 <input type="radio" name="ContactsTabSelect" />
                 на машине
             </label>
-            <label className="ContactsTabSelect__option">
+            <label className="ContactsTabSelectPanel__option">
                 <input type="radio" name="ContactsTabSelect" />
                 на такси
             </label>
