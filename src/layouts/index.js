51449982dd4420e9e5ca51e7c9f261746c494617
Oleg Legun <oleg.legun@gmail.com>
2017-09-21 16:45:05 +0300

    add default template

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,64 +1,32 @@
+import 'normalize.css'
+import '../css/styles.less'
 import React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import PageHeader from '../components/PageHeader'
+import PageContent from '../components/PageContent'
+import PageFooter from '../components/PageFooter'
+import MobileMenu from '../components/MobileMenu'
 
-import './index.css'
+function Page({ children }) {
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
-
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
-
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+    return (
+        <div id="outer-container">
+            <MobileMenu className="MobileMenu" right width={250} pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+            <div id="page-wrap" className="Page">
+                <div className="Page__wrapper">
+                    <PageHeader />
+                    <PageContent>
+                        {children()}
+                    </PageContent>
+                    <PageFooter />
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default TemplateWrapper
+Page.propTypes = {
+    children: PropTypes.object.isRequired  // eslint-disable-line react/forbid-prop-types
+}
+
+export default Page
