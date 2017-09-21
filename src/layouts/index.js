e37d092772e9204a3b81fb7d28b60fac0935201a
Oleg Legun <oleg.legun@gmail.com>
2017-09-21 16:55:48 +0300

    make dirs for components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import 'normalize.css'
 import '../css/styles.less'
 import React from 'react'
 import PropTypes from 'prop-types'
+
 import PageHeader from '../components/PageHeader'
 import PageContent from '../components/PageContent'
 import PageFooter from '../components/PageFooter'
@@ -11,7 +12,10 @@ function Page({ children }) {
 
     return (
         <div id="outer-container">
-            <MobileMenu className="MobileMenu" right width={250} pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+            <MobileMenu
+                className="MobileMenu" right width={250}
+                pageWrapId={'page-wrap'}
+                outerContainerId={'outer-container'} />
             <div id="page-wrap" className="Page">
                 <div className="Page__wrapper">
                     <PageHeader />
