1ec0f058e4c8dafeb27a0787f8d898e2f7f84243
Oleg Legun <oleg.legun@gmail.com>
2017-10-11 02:12:25 +0300

    update img paths

--- src/files/config/contacts-path-images.js
+++ src/files/config/contacts-path-images.js
@@ -1,97 +1,97 @@
 const commonImages = [
     {
-        src: '/lasertime/img/contacts/path-common-1.jpg',
+        src: '/img/contacts/path-common-1.jpg',
         w: 1000,
         h: 756,
         title:
             'Выйти на станции "Сретенский Бульвар" и идти в сторону эскалатора',
     },
     {
-        src: '/lasertime/img/contacts/path-common-2.jpg',
+        src: '/img/contacts/path-common-2.jpg',
         w: 1000,
         h: 750,
         title: 'Идти в сторону эскалатора на выход в город',
     },
     {
-        src: '/lasertime/img/contacts/path-common-3.jpg',
+        src: '/img/contacts/path-common-3.jpg',
         w: 1000,
         h: 750,
         title: 'Продолжать движение на выход в город к Сретенскому бульвару',
     },
     {
-        src: '/lasertime/img/contacts/path-common-4.jpg',
+        src: '/img/contacts/path-common-4.jpg',
         w: 1000,
         h: 750,
         title:
             'Продолжать движение в сторону выхода в город к Сретенскому бульвару',
     },
     {
-        src: '/lasertime/img/contacts/path-common-5.jpg',
+        src: '/img/contacts/path-common-5.jpg',
         w: 1000,
         h: 750,
         title:
             'Подняться на эскалаторе на выход в город к Сретенскому бульвару',
     },
     {
-        src: '/lasertime/img/contacts/path-common-6.jpg',
+        src: '/img/contacts/path-common-6.jpg',
         w: 1000,
         h: 750,
         title:
             'Подняться на эскалаторе на выход в город к Сретенскому бульвару',
     },
     {
-        src: '/lasertime/img/contacts/path-common-7.jpg',
+        src: '/img/contacts/path-common-7.jpg',
         w: 1000,
         h: 825,
         title: 'Выйти через турникеты в подземный переход и повернуть направо',
     },
     {
-        src: '/lasertime/img/contacts/path-common-8.jpg',
+        src: '/img/contacts/path-common-8.jpg',
         w: 1000,
         h: 742,
         title: 'При выходе из стеклянных дверей метро – повернуть направо',
     },
     {
-        src: '/lasertime/img/contacts/path-common-9.jpg',
+        src: '/img/contacts/path-common-9.jpg',
         w: 1000,
         h: 854,
         title:
             'Продолжать движение прямо по подземному переходу до стеклянных палаток',
     },
     {
-        src: '/lasertime/img/contacts/path-common-10.jpg',
+        src: '/img/contacts/path-common-10.jpg',
         w: 1000,
         h: 750,
         title: 'Дойдя до стеклянных палаток – повернуть налево',
     },
     {
-        src: '/lasertime/img/contacts/path-common-11.jpg',
+        src: '/img/contacts/path-common-11.jpg',
         w: 1000,
         h: 764,
         title:
             'Продолжать движение вдоль стеклянных палаток (по правую руку) до конца перехода и подняться по ступеням',
     },
     {
-        src: '/lasertime/img/contacts/path-common-12.jpg',
+        src: '/img/contacts/path-common-12.jpg',
         w: 1000,
         h: 716,
         title: 'Подняться по ступеням (выход №8)',
     },
     {
-        src: '/lasertime/img/contacts/path-common-13.jpg',
+        src: '/img/contacts/path-common-13.jpg',
         w: 1000,
         h: 770,
         title:
             'Подняться по ступеням и продолжать движение прямо по Сретенскому бульвару',
     },
     {
-        src: '/lasertime/img/contacts/path-common-14.jpg',
+        src: '/img/contacts/path-common-14.jpg',
         w: 1000,
         h: 818,
         title: 'Двигаться прямо по Сретенскому бульвару',
     },
     {
-        src: '/lasertime/img/contacts/path-common-15.jpg',
+        src: '/img/contacts/path-common-15.jpg',
         w: 1000,
         h: 750,
         title: 'Продолжать движение прямо',
@@ -102,63 +102,63 @@ const PathImages = {
     top: [
         ...commonImages,
         {
-            src: '/lasertime/img/contacts/path-top-1.jpg',
+            src: '/img/contacts/path-top-1.jpg',
             w: 1300,
             h: 741,
             title:
                 'Не доходя до аптеки 36.6, свернуть направо на Костянский переулок',
         },
         {
-            src: '/lasertime/img/contacts/path-top-2.jpg',
+            src: '/img/contacts/path-top-2.jpg',
             w: 1000,
             h: 1000,
             title:
                 'Продолжать движение прямо до 1-го поворота налево (Рыбников переулок)',
         },
         {
-            src: '/lasertime/img/contacts/path-top-3.jpg',
+            src: '/img/contacts/path-top-3.jpg',
             w: 1000,
             h: 1000,
             title: 'Свернуть налево – в Рыбников переулок',
         },
         {
-            src: '/lasertime/img/contacts/path-top-4.jpg',
+            src: '/img/contacts/path-top-4.jpg',
             w: 1000,
             h: 1000,
             title: 'Двигаться прямо по переулку',
         },
         {
-            src: '/lasertime/img/contacts/path-top-5.jpg',
+            src: '/img/contacts/path-top-5.jpg',
             w: 1000,
             h: 1000,
             title: 'Пройти вдоль оранжевого забора до калитки (по левую руку)',
         },
         {
-            src: '/lasertime/img/contacts/path-top-6.jpg',
+            src: '/img/contacts/path-top-6.jpg',
             w: 1000,
             h: 750,
             title: 'Войти в калитку (всегда открыта)',
         },
         {
-            src: '/lasertime/img/contacts/path-top-7.jpg',
+            src: '/img/contacts/path-top-7.jpg',
             w: 1000,
             h: 739,
             title: 'Войти во двор, дойти до дома и свернуть направо',
         },
         {
-            src: '/lasertime/img/contacts/path-top-8.jpg',
+            src: '/img/contacts/path-top-8.jpg',
             w: 1000,
             h: 1000,
             title: 'Пройди до конца дома',
         },
         {
-            src: '/lasertime/img/contacts/path-top-9.jpg',
+            src: '/img/contacts/path-top-9.jpg',
             w: 1000,
             h: 894,
             title: 'Войти к клинику красоты "Знак Качества"',
         },
         {
-            src: '/lasertime/img/contacts/path-finish.jpg',
+            src: '/img/contacts/path-finish.jpg',
             w: 1000,
             h: 810,
             title: 'Добро пожаловать! Вы на месте!',
@@ -167,59 +167,59 @@ const PathImages = {
     bottom: [
         ...commonImages,
         {
-            src: '/lasertime/img/contacts/path-bottom-1.jpg',
+            src: '/img/contacts/path-bottom-1.jpg',
             w: 1300,
             h: 741,
             title:
                 'Продолжать движение по Сретенскому бульвару (аптека 36.6 по правую руку)',
         },
         {
-            src: '/lasertime/img/contacts/path-bottom-2.jpg',
+            src: '/img/contacts/path-bottom-2.jpg',
             w: 1000,
             h: 1000,
             title: 'Продолжать движение по Сретенскому бульвару',
         },
         {
-            src: '/lasertime/img/contacts/path-bottom-3.jpg',
+            src: '/img/contacts/path-bottom-3.jpg',
             w: 1000,
             h: 1000,
             title: 'Продолжать движение по Сретенскому бульвару',
         },
         {
-            src: '/lasertime/img/contacts/path-bottom-4.jpg',
+            src: '/img/contacts/path-bottom-4.jpg',
             w: 1000,
             h: 1000,
             title:
                 'Продолжать движение по Сретенскому бульвару (Почта Банк, Почта России по правую руку)',
         },
         {
-            src: '/lasertime/img/contacts/path-bottom-5.jpg',
+            src: '/img/contacts/path-bottom-5.jpg',
             w: 1000,
             h: 824,
             title:
                 'Дойти до гостиницы "Связист" (Сретенский бульвар, д. 5) и за ней свернуть направо в проход между домами',
         },
         {
-            src: '/lasertime/img/contacts/path-bottom-6.jpg',
+            src: '/img/contacts/path-bottom-6.jpg',
             w: 700,
             h: 933,
             title:
                 'Подойти к железной двери, позвонить в красный домофон – Вам откроют',
         },
         {
-            src: '/lasertime/img/contacts/path-bottom-7.jpg',
+            src: '/img/contacts/path-bottom-7.jpg',
             w: 1000,
             h: 648,
             title: 'Зайти в калитку и сразу повернуть налево',
         },
         {
-            src: '/lasertime/img/contacts/path-bottom-8.jpg',
+            src: '/img/contacts/path-bottom-8.jpg',
             w: 1000,
             h: 930,
             title: 'Войти к клинику красоты "Знак Качества"',
         },
         {
-            src: '/lasertime/img/contacts/path-finish.jpg',
+            src: '/img/contacts/path-finish.jpg',
             w: 1000,
             h: 810,
             title: 'Добро пожаловать! Вы на месте!',
