89a4d31b0de0d65bad858e2caab729f0719836a4
Oleg Legun <oleg.legun@gmail.com>
2017-10-13 22:18:23 +0300

    add styles for ArticlePreview

--- src/components/ArticlePreview/index.js
+++ src/components/ArticlePreview/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import articleIcon from './src/icon-article.svg'
+import interviewIcon from './src/icon-interview.svg'
 
 function ArticlePreview({ type, image, title, description, path }) {
+    const iconSrc = type === 'interview' ? interviewIcon : articleIcon
+
     return (
         <div className="ArticlePreview">
             <div
@@ -10,12 +14,16 @@ function ArticlePreview({ type, image, title, description, path }) {
                     type
                 }`}
             >
-                <div className="ArticlePreview__article-type-icon">Icon</div>
+                <div className="ArticlePreview__article-type-icon">
+                    <img src={iconSrc} />
+                </div>
                 <div className="ArticlePreview__article-type-text">Text</div>
             </div>
             <div className="ArticlePreview__img">image</div>
             <div className="ArticlePreview__content">
-                <div className="ArticlePreview__title">{title}</div>
+                <div className="ArticlePreview__title">
+                    <span>{title}</span>
+                </div>
                 <div className="ArticlePreview__description">{description}</div>
                 <div className="ArticlePreview__link">
                     <Link to={path}>Читать полностью</Link>
@@ -33,6 +41,10 @@ ArticlePreview.propTypes = {
     path: PropTypes.string.isRequired,
 }
 
-ArticlePreview.defaultProps = {}
+ArticlePreview.defaultProps = {
+    type: 'article',
+    description:
+        'Nunc id mi vitae tellus semper tincidunt eget et eros. Phasellus placerat tellus quis sapien bibendum, ac tincidunt enim ornare. Etiam cursus justo eu orci consequat, sit amet pellentesque ante blandit.',
+}
 
 export default ArticlePreview
