b21f97e8f817d0e861140a1d8604cc835096bea2
Oleg Legun <oleg.legun@gmail.com>
2017-10-14 02:57:21 +0300

    Update styles for ArticlePreview

--- src/components/ArticlePreview/index.js
+++ src/components/ArticlePreview/index.js
@@ -3,38 +3,47 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import articleIcon from './src/icon-article.svg'
 import interviewIcon from './src/icon-interview.svg'
+import interviewTypeText from './src/text-interview.svg'
+import articleTypeText from './src/text-article.svg'
 
 function ArticlePreview({ type, image, title, description, path }) {
-    const iconSrc = type === 'interview' ? interviewIcon : articleIcon
+    const TypeIcon = type === 'interview' ? interviewIcon : articleIcon
+    const TypeText = type === 'interview' ? interviewTypeText : articleTypeText
 
     return (
-        <div className="ArticlePreview">
-            <div
-                className={`ArticlePreview__article-type ArticlePreview__article-type--${
-                    type
-                }`}
-            >
-                <div className="ArticlePreview__article-type-icon">
-                    <img src={iconSrc} />
+        <Link to={path}>
+            <div className="ArticlePreview">
+                <div
+                    className={`ArticlePreview__article-type ArticlePreview__article-type--${
+                        type
+                    }`}
+                >
+                    <div className="ArticlePreview__article-type-icon">
+                        <img src={TypeIcon} />
+                    </div>
+                    <div className="ArticlePreview__article-type-text">
+                        <img src={TypeText} />
+                    </div>
                 </div>
-                <div className="ArticlePreview__article-type-text">Text</div>
+                <div className="ArticlePreview__img">
+                    <img src={image} />
+                </div>
+                <div className="ArticlePreview__content">
+                    <div className="ArticlePreview__title">
+                        <span>{title}</span>
+                    </div>
+                    <div className="ArticlePreview__description">
+                        {description}
+                    </div>
+                    <div className="ArticlePreview__link">Читать полностью</div>
+                </div>
             </div>
-            <div className="ArticlePreview__img">image</div>
-            <div className="ArticlePreview__content">
-                <div className="ArticlePreview__title">
-                    <span>{title}</span>
-                </div>
-                <div className="ArticlePreview__description">{description}</div>
-                <div className="ArticlePreview__link">
-                    <Link to={path}>Читать полностью</Link>
-                </div>
-            </div>
-        </div>
+        </Link>
     )
 }
 
 ArticlePreview.propTypes = {
-    type: PropTypes.oneOf('article', 'interview'),
+    type: PropTypes.oneOf(['article', 'interview']),
     image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
@@ -43,6 +52,7 @@ ArticlePreview.propTypes = {
 
 ArticlePreview.defaultProps = {
     type: 'article',
+    image: 'https://placeimg.com/180/180/any',
     description:
         'Nunc id mi vitae tellus semper tincidunt eget et eros. Phasellus placerat tellus quis sapien bibendum, ac tincidunt enim ornare. Etiam cursus justo eu orci consequat, sit amet pellentesque ante blandit.',
 }
