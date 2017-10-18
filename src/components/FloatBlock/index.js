8b901721003528cd75c01bc616cf35fe3e441b19
Oleg Legun <oleg.legun@gmail.com>
2017-10-18 17:40:46 +0300

    add FloatBlock component

---
+++ src/components/FloatBlock/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function FloatBlock({ left, right, oneThird, children }) {
+    let floatClass
+
+    floatClass = left ? 'FloatBlock--left' : 'FloatBlock--right'
+
+    if (oneThird) {
+        floatClass += ' FloatBlock--one-third'
+    }
+
+    return <div className={`FloatBlock ${floatClass}`}>{children}</div>
+}
+
+FloatBlock.propTypes = {
+    left: PropTypes.bool,
+    right: PropTypes.bool,
+    oneThird: PropTypes.bool,
+}
+
+FloatBlock.defaultProps = {
+    left: false,
+    oneThird: false,
+}
+
+export default FloatBlock
