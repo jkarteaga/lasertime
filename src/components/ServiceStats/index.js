7e425006c626627deffce222a7ab19428b455434
Oleg Legun <oleg.legun@gmail.com>
2017-11-11 21:14:17 +0300

    add styles for ServiceStats

--- src/components/ServiceStats/index.js
+++ src/components/ServiceStats/index.js
@@ -13,53 +13,56 @@ function ServiceStats({
 }) {
     return (
         <div className={'ServiceStats'}>
-            <div className="ServiceStats__item">
-                <div className="ServiceStats__icon ServiceStats__icon--duration" />
-                <div className="ServiceStats__body">
-                    <div className="ServiceStats__heading">
-                        Длительность процедуры
-                    </div>
-                    <div className="ServiceStats__text">{durationText}</div>
-                    <div className="ServiceStats__link">
-                        {durationModal ? (
-                            <Modal
-                                text={'Подробнее'}
-                                title={'Длительность процедуры'}
-                            >
-                                {durationModal}
-                            </Modal>
-                        ) : null}
-                    </div>
-                </div>
-            </div>
-            <div className="ServiceStats__item">
-                <div className="ServiceStats__icon ServiceStats__icon--money" />
-                <div className="ServiceStats__body">
-                    <div className="ServiceStats__heading">
-                        Стоимость процедуры
-                    </div>
-                    <div className="ServiceStats__text">{priceText}</div>
-                    <div className="ServiceStats__link">
-                        <Link to={priceLink}>Узнать цены</Link>
+            <h2 className={'ServiceStats__heading'}>Статистика</h2>
+            <div className="ServiceStats__body">
+                <div className="ServiceStats__item">
+                    <div className="ServiceStats__icon ServiceStats__icon--duration" />
+                    <div className="ServiceStats__item-body">
+                        <div className="ServiceStats__item-heading">
+                            Длительность процедуры
+                        </div>
+                        <div className="ServiceStats__text">{durationText}</div>
+                        <div className="ServiceStats__link">
+                            {durationModal ? (
+                                <Modal
+                                    text={'Подробнее'}
+                                    title={'Длительность процедуры'}
+                                >
+                                    {durationModal}
+                                </Modal>
+                            ) : null}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="ServiceStats__item">
-                <div className="ServiceStats__icon ServiceStats__icon--quantity" />
-                <div className="ServiceStats__body">
-                    <div className="ServiceStats__heading">
-                        Кол-во процедур в курсе
+                <div className="ServiceStats__item">
+                    <div className="ServiceStats__icon ServiceStats__icon--money" />
+                    <div className="ServiceStats__item-body">
+                        <div className="ServiceStats__item-heading">
+                            Стоимость процедуры
+                        </div>
+                        <div className="ServiceStats__text">{priceText}</div>
+                        <div className="ServiceStats__link">
+                            <Link to={priceLink}>Узнать цены</Link>
+                        </div>
                     </div>
-                    <div className="ServiceStats__text">{quantityText}</div>
-                    <div className="ServiceStats__link">
-                        {quantityModal ? (
-                            <Modal
-                                text={'Подробнее'}
-                                title={'Кол-во процедур в курсе'}
-                            >
-                                {quantityModal}
-                            </Modal>
-                        ) : null}
+                </div>
+                <div className="ServiceStats__item">
+                    <div className="ServiceStats__icon ServiceStats__icon--quantity" />
+                    <div className="ServiceStats__item-body">
+                        <div className="ServiceStats__item-heading">
+                            Количество процедур в курсе
+                        </div>
+                        <div className="ServiceStats__text">{quantityText}</div>
+                        <div className="ServiceStats__link">
+                            {quantityModal ? (
+                                <Modal
+                                    text={'Подробнее'}
+                                    title={'Количество процедур в курсе'}
+                                >
+                                    {quantityModal}
+                                </Modal>
+                            ) : null}
+                        </div>
                     </div>
                 </div>
             </div>
