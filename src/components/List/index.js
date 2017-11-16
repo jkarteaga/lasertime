bbcf36d66b56a66d1c5e9d533ec214a5522a44c0
Oleg Legun <oleg.legun@gmail.com>
2017-11-16 17:42:00 +0300

    add title options to List component

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function List({ marker, children }) {
-    return <div className={`List List--marker-${marker}`}>{children}</div>
+function List({ title, titleH2, titleH3, marker, children }) {
+    return (
+        <div className={`List List--marker-${marker}`}>
+            {title ? <div className="List__title">{title}</div> : null}
+            {titleH2 ? (
+                <h2 className="List__title List__title--h2">{titleH2}</h2>
+            ) : null}
+            {titleH3 ? (
+                <h3 className="List__title List__title--h3">
+                    <span>{titleH3}</span>
+                </h3>
+            ) : null}
+            {children}
+        </div>
+    )
 }
 
 List.propTypes = {
+    title: PropTypes.string,
+    titleH2: PropTypes.string,
+    titleH3: PropTypes.string,
     marker: PropTypes.oneOf(['check', 'plus', 'minus', 'cross']),
 }
 
