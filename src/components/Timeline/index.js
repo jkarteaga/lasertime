1ee4a06732f610b292f9e50a58c77192e55d0ced
Oleg Legun <oleg.legun@gmail.com>
2017-11-19 18:16:03 +0300

    add desktop styles for Timeline

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function Timeline({ children }) {
+function Timeline({ title, children }) {
     return (
         <div className="Timeline">
+            <h2 className="Timeline__title">{title}</h2>
             <ul className="Timeline__list">{children}</ul>
         </div>
     )
 }
 
 Timeline.propTypes = {
+    title: PropTypes.string.isRequired,
     children: PropTypes.node.isRequired,
 }
 
@@ -18,9 +20,13 @@ export default Timeline
 export const TimelineItem = ({ date, name, description }) => {
     return (
         <li className="TimelineItem">
-            <h3 className="TimelineItem__date">{date}</h3>
-            <p className="TimelineItem__name">{name}</p>
-            <p className="TimelineItem__description">{description}</p>
+            <div className="TimelineItem__header">
+                <h3 className="TimelineItem__date">{date}</h3>
+            </div>
+            <div className="TimelineItem__body">
+                <p className="TimelineItem__name">{name}</p>
+                <p className="TimelineItem__description">{description}</p>
+            </div>
         </li>
     )
 }
