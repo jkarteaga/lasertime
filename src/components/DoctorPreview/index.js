846d4ab539ca2aa96a3aa8b65e64d59aadd9638f
Oleg Legun <oleg.legun@gmail.com>
2017-11-21 14:02:35 +0300

    add DoctorPreview component

---
+++ src/components/DoctorPreview/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Img from 'gatsby-image'
+
+function DoctorPreview({ name, position, imageSizes }) {
+    return (
+        <div className="DoctorPreview">
+            <h2>{name}</h2>
+            <div>{position}</div>
+            <div className="DoctorPreview__image">
+                {/*<Img sizes={imageSizes} />*/}
+            </div>
+        </div>
+    )
+}
+
+DoctorPreview.propTypes = {
+    name: PropTypes.string.isRequired,
+    position: PropTypes.string.isRequired,
+    imageSizes: PropTypes.object.isRequired,
+}
+
+DoctorPreview.defaultProps = {}
+
+export default DoctorPreview
