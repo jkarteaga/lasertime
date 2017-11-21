76bf36647fda92cd18838560251709fa649df8ec
Oleg Legun <oleg.legun@gmail.com>
2017-11-21 15:02:30 +0300

    update doctors page

--- src/pages/doctors.js
+++ src/pages/doctors.js
@@ -1,29 +1,45 @@
 import React from 'react'
 import Helmet from '../components/HelmetWrapper'
-import PagePreview from '../components/PagePreview'
 import ArticleHeaderImage from '../components/ArticleHeaderImage'
+import DoctorPreview from '../components/DoctorPreview'
+import Link from 'gatsby-link'
 
 function Doctors({ data }) {
-    const doctorsElements = data.allMarkdownRemark.edges.map(({ node }) => {
-        return (
-            <PagePreview
-                key={node.internal.contentDigest}
-                title={node.frontmatter.title}
-                path={node.frontmatter.path}
-            />
-        )
-    })
-
     return (
         <div>
             <Helmet data={data} title="Врачи" description="" />
             <ArticleHeaderImage
                 title={'Врачи'}
-                imgSizes={data.blank.sizes}
+                imgSizes={data.doctorsHeader.sizes}
                 alignLeft
                 noEffects
             />
-            <div className="PageContent__wrapper">{doctorsElements}</div>
+            <div className="PageContent__wrapper">
+                <Link to="/doctors/legun-alla">
+                    <DoctorPreview
+                        name="Легун Алла Александровна"
+                        position="главный врач"
+                    />
+                </Link>
+                <Link to="/doctors/portnova-ekaterina">
+                    <DoctorPreview
+                        name="Портнова Екатерина Вячеславовна"
+                        position="специалист по лазерным и световым технологиям"
+                    />
+                </Link>
+                <Link to="/doctors/dolidze-tatiana">
+                    <DoctorPreview
+                        name="Долидзе Татьяна Джумберовна"
+                        position="ассистент врача-косметолога"
+                    />
+                </Link>
+                <Link to="/doctors/gvaramiya-aleksey">
+                    <DoctorPreview
+                        name="Гварамия Алексей Алекович"
+                        position="пластический хирург"
+                    />
+                </Link>
+            </div>
         </div>
     )
 }
@@ -37,22 +53,9 @@ export default Doctors
 export const pageQuery = graphql`
     query Doctors {
         ...Helmet
-        blank: imageSharp(id: { regex: "/doctors-header/" }) {
+        doctorsHeader: imageSharp(id: { regex: "/doctors-header/" }) {
             sizes(maxWidth: 960, quality: 65) {
                 ...GatsbyImageSharpSizes_noBase64
-            }
-        }
-        allMarkdownRemark(filter: { fields: { group: { eq: "doctors" } } }) {
-            edges {
-                node {
-                    internal {
-                        contentDigest
-                    }
-                    frontmatter {
-                        title
-                        path
-                    }
-                }
             }
         }
     }
