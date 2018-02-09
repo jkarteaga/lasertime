db95a23e01ba30e7470abb39d2d4068e56d0f833
Oleg Legun <oleg.legun@gmail.com>
2018-02-09 20:22:35 +0300

    add ServiceList component

---
+++ src/components/ServiceList/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function ServiceList({ listItems }) {
+    const listElements = listItems.map(item => (
+        <li className="ServiceList__item">{item}</li>
+    ))
+
+    return <ul className="ServiceList">{listElements}</ul>
+}
+
+ServiceList.propTypes = {
+    listItems: PropTypes.array.isRequired,
+}
+
+ServiceList.defaultProps = {}
+
+export default ServiceList
